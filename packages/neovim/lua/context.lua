3206c0645d1fcd338a0cb99e3a25e49b7126d621
Jake Hamilton <jake.hamilton@hey.com>
2023-06-29 11:28:05 -0700

    fix: temporarily disable ts context

--- packages/neovim/lua/context.lua
+++ packages/neovim/lua/context.lua
@@ -1,22 +1,25 @@
 local context = require("treesitter-context")
 local which_key = require("which-key")
 
-context.setup {
-	enable = true,
-	max_lines = 10,
-	line_numbers = true,
-	trim_scope = "outer",
-	mode = "topline",
-}
+-- @FIXME(jakehamilton): Enable this again when this issue is fixed:
+-- https://github.com/neovim/neovim/issues/19464
+-- context.setup {
+-- 	enable = true,
+-- 	max_lines = 10,
+-- 	line_numbers = true,
+-- 	trim_scope = "outer",
+-- 	mode = "topline",
+-- }
 
-which_key.register({
-	g = {
-		name = "Go",
-		C = {
-			function()
-				context.go_to_context()
-			end,
-			"Go to context"
-		},
-	},
-}, { mode = "n", noremap = true, silent = true })
+-- which_key.register({
+-- 	g = {
+-- 		name = "Go",
+-- 		C = {
+-- 			function()
+-- 				context.go_to_context()
+-- 			end,
+-- 			"Go to context"
+-- 		},
+-- 	},
+-- }, { mode = "n", noremap = true, silent = true })
+--
